75fb2f54459802e1aaf40982a978a42b77149907
Lala Lala Haha HiHi <103634965+RozhBasXYZ@users.noreply.github.com>
2023-03-17 20:13:19 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,2 @@
-#hai kang rekode, kapan skil mu berkembang?
-
-
-try:
-   from data.liteblade import main as ngentot
-   ngentot()
-except:
-   from data.nextblade32 import make as adik
-   adik()
+from data.liteblade import main
+main()
