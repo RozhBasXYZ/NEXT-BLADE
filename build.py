31e6b018a99f24145330c6ae2fb6b1e563f9323b
RozhBasXYZ <103634965+RozhBasXYZ@users.noreply.github.com>
2024-02-18 15:48:31 +0700

    Update build.py

--- build.py
+++ build.py
@@ -6,15 +6,14 @@ for main_file in ["blade.cpp", "menu.cpp"]:
 	try:
 		nama = main_file.split(".")[0]
 		setup( name = main_file, ext_modules = cythonize(Extension(name=nama, sources=[main_file])), script_args = ["build_ext", "--inplace", "--force", "-j 5"])
-	except Exception as e: pass
-	
+	except Exception as e: pass	
+		
+for main_file in ["blade.cpp", "menu.cpp"]:
 	if os.path.exists(main_file):
 		try: shutil.rmtree("build/")
 		except: 0
 	else:
 		os.system("pip install -r requirements.txt")
 		os.system("python build.py")
-		
-for main_file in ["blade.cpp", "menu.cpp"]:
 	try: os.remove(main_file)
-	except: 0
+	except: 0	
